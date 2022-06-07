86c2373c703460ec8daec0051c817db539d7d7ce
Saroj Kumar <kumarsaroj18@gmail.com>
2022-06-07 09:48:43 +0530

    Created project skeleton

---
+++ src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -0,0 +1,11 @@
+package com.optimagrowth.license;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LicenseServiceApplication.class, args);
+    }
+}
